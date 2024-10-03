95833857246cccaad4b54b6d7b13aede173f5eb9
Jordan Matelsky <j6k4m8@gmail.com>
2024-10-03 16:14:03 -0500

    feat: switch to enum, it's the way to do this

--- src/lib.rs
+++ src/lib.rs
@@ -1,116 +1,120 @@
-/// FSTreeMap is a tree structure that represents the file system.
-///
-/// It is distinct from the _actual_ file system in that it can represent a
-/// fully virtual or otherwise imaginary, abstracted file system.
-///
-/// The tree is a bidirected graph, with each node representing a file or
-/// directory. Both files and directories are Nodes, but directories have
-/// children, which are also Nodes.
-///
-/// The File (leaf) is implemented by files, and the Directory trait is
-/// implemented by directories. The Node is implemented by both.
-/// Only leaves have data, and only directories have children.
-///
-/// Values are generickized, so you can store any type of data in the tree.
-
-/// Nodes are a container for either files or directories.
-trait Node<V> {
-    /// Returns the name of the node.
-    fn name(&self) -> &str;
+#[derive(Clone)]
+pub enum Node {
+    Directory {
+        name: String,
+        children: Vec<Box<Node>>,
+    },
+    File {
+        name: String,
+        size: usize,
+    },
 }
-
-/// Files are leaves in the tree:
-struct FileNode<V> {
-    name: String,
-    data: V,
-}
-
-impl<V> Node<V> for FileNode<V> {
-    fn name(&self) -> &str {
-        &self.name
-    }
-}
-
-/// Directories are nodes with children.
-struct DirectoryNode<V> {
-    name: String,
-    children: Vec<Box<dyn Node<V>>>,
-}
-
-impl<V> Node<V> for DirectoryNode<V> {
-    fn name(&self) -> &str {
-        &self.name
-    }
-}
-
-impl<V> DirectoryNode<V> {
-    fn new(name: &str) -> Self {
-        DirectoryNode {
-            name: name.to_string(),
-            children: Vec::<Box<dyn Node<V>>>::new(),
+impl Node {
+    pub fn get_name(&self) -> &String {
+        match self {
+            Node::Directory { name, .. } => name,
+            Node::File { name, .. } => name,
         }
     }
 
-    fn add_child(&mut self, child: Box<dyn Node<V>>) {
-        self.children.push(child);
+    pub fn get_child(&self, name: &str) -> Option<&Box<Node>> {
+        match self {
+            Node::Directory { children, .. } => {
+                children.iter().find(|child| child.get_name() == name)
+            }
+            Node::File { .. } => None,
+        }
     }
 
-    fn remove_child(&mut self, name: &str) {
-        self.children.retain(|child| child.name() != name);
+    pub fn get_mut_child(&mut self, name: &str) -> Option<&mut Box<Node>> {
+        match self {
+            Node::Directory { ref mut children, .. } => {
+                children.iter_mut().find(|child| child.get_name() == name)
+            }
+            Node::File { .. } => None,
+        }
     }
 
-    fn get_child(&self, name: &str) -> Option<&Box<dyn Node<V>>> {
-        self.children.iter().find(|child| child.name() == name)
-    }
-
-    fn get_children(&self) -> &Vec<Box<dyn Node<V>>> {
-        &self.children
+    pub fn get_value(&self) -> usize {
+        match self {
+            Node::File {size,..} => *size,
+            Node::Directory {  children,.. } => {
+                children.iter().map(|child| child.get_value()).sum()
+            }
+        }
     }
 }
 
-/// The tree itself.
-struct FSTreeMap<V> {
-    root: Box<DirectoryNode<V>>,
+pub struct FSTreeMap {
+    pub(crate) root: Box<Node>,
 }
 
-/// Implement the tree.
-impl<V: 'static> FSTreeMap<V> {
-    fn new() -> Self {
+impl FSTreeMap {
+    pub fn new() -> Self {
         FSTreeMap {
-            root: Box::new(DirectoryNode::new("root")),
+            root: Box::new(Node::Directory {
+                name: String::from("root"),
+                children: vec![],
+            }),
         }
     }
 
-    fn add_child_to_path(
-        &mut self,
-        path: Vec<&str>,
-        child: Box<dyn Node<V>>,
-        create_parents: bool,
-    ) {
-        // Start at the root.
-        let current = &mut self.root;
-        // For each subdir in the path:
-        for name in path {
-            // Get the child if it exists, or panic/create:
-            let child = match current.get_child(name) {
-                Some(child) => child,
-                None => {
-                    if create_parents {
-                        let new_child = Box::new(DirectoryNode::new(name));
-                        current.add_child(new_child);
-                        current.get_child(name).unwrap()
-                    } else {
-                        panic!("Path not found.")
+    pub fn get_size(&self, path: &str) -> usize {
+        match self.get_node(path).unwrap() {
+            Node::File { size, .. } => *size,
+            _ => panic!("no size defined on directories"),
+        }
+    }
+
+    pub fn get_node(&self, path: &str) -> Option<&Node> {
+        let mut current = &self.root;
+        for part in path.split('/') {
+            match **current {
+                Node::Directory { ref children, .. } => {
+                    match children.iter().find(|child| {
+                        let node = child.as_ref();
+                        let name = node.get_name();
+                        return *name == String::from(part);
+                    }) {
+                        Some(child) => {
+                            current = child;
+                        }
+                        None => return None,
                     }
                 }
-            };
-
-            // If the child is a directory, continue to traverse deeper.
+                _ => panic!("Path is not a directory"),
+            }
         }
-        // Add the child to the directory.
-        current.add_child(child);
+        Some(current)
+    }
+
+    pub fn insert(&mut self, path: &str, size: usize) {
+        let mut current = &mut self.root;
+        let full_path_split: Vec<&str> = path.split('/').into_iter().collect();
+        let dirpath = &full_path_split[..full_path_split.len() - 1];
+        let stem = full_path_split.last().unwrap();
+        for part in dirpath {
+            current = current.get_mut_child(part).unwrap();
+        }
+
+        match **current {
+            Node::Directory {
+                ref mut children, ..
+            } => {
+                let new_node = Node::File {
+                    name: stem.to_string(),
+                    size,
+                };
+
+                children.push(Box::new(new_node));
+            }
+
+            Node::File { .. } => panic!("Path already exists"),
+        }
     }
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -118,40 +122,25 @@ mod tests {
 
     #[test]
     fn can_create_file_node() {
-        let file = FileNode {
+        let file = Node::File {
             name: "answer.txt".to_string(),
-            data: 42,
+            size: 42,
         };
-        assert_eq!(file.name, "answer.txt");
-        assert_eq!(file.data, 42);
+        assert_eq!(file.get_name(), "answer.txt");
+        assert_eq!(file.get_value(), 42);
     }
 
     #[test]
     fn can_create_directory_node() {
-        let dir: DirectoryNode<i32> = DirectoryNode::new("home");
-        assert_eq!(dir.name, "home");
-    }
-
-    #[test]
-    fn can_add_child_to_directory_node() {
-        let mut dir: DirectoryNode<i32> = DirectoryNode::new("home");
-        let child = Box::new(FileNode {
-            name: "answer.txt".to_string(),
-            data: 42,
-        });
-        dir.add_child(child);
-        assert_eq!(dir.get_children().len(), 1);
+        let dir  = Node::Directory { name: "home".to_string(), children: vec![] };
+        assert_eq!(dir.get_name(), "home");
     }
 
     #[test]
     fn test_add_child_to_path() {
         let mut tree = FSTreeMap::new();
-        let child = Box::new(FileNode {
-            name: "answer.txt".to_string(),
-            data: 42,
-        });
-        tree.add_child_to_path(vec!["home", "users", "arthur"], child, true);
+        tree.insert("home",  42);
 
-        assert_eq!(tree.root.get_child("home").unwrap().name(), "home");
+        assert_eq!(tree.root.get_value(), 42);
     }
 }
